0467bdf23820cf405289333e68f1b2185438714b
pnolan89 <petenol@gmail.com>
2019-02-22 04:16:31 +0000

    more setup for review submit form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,13 @@
 class ProductsController < ApplicationController
 
-  def index
+  def index #WOULD BE TRANLATED TO THE /PRODUCTS GET
     @products = Product.all.order(created_at: :desc)
+
   end
 
   def show
     @product = Product.find params[:id]
+    @review = Review.new
   end
 
 end
