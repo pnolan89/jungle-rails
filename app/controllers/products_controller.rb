ab8d47e1540b7e9f7532fdcde9775828c5a21a63
pnolan89 <petenol@gmail.com>
2019-02-22 21:15:06 +0000

    add create/delete methods for reviews controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :require_login
 
   def index #WOULD BE TRANLATED TO THE /PRODUCTS GET
     @products = Product.all.order(created_at: :desc)
@@ -8,6 +9,14 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Review.new
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to post reviews."
+    end
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,42 @@
 class ReviewsController < ApplicationController
+  before_action :require_login
+
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = params[:product_id]
+    @product = Product.find(params[:product_id])
+    puts 'REVIEW OBJECT:'
+    puts @review
+    puts @review.user_id
+    if @review.save
+      redirect_to controller: 'products', action: 'show', id: params[:product_id], notice: 'Review saved!'
+    else
+      redirect_to controller: 'products', action: 'show', id: params[:product_id], notice: 'Not saved...'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+
+    redirect_to controller: 'products', action: 'show', id: params[:product_id]
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to post reviews."
+      redirect_to controller: 'products', action: 'show', id: params[:product_id]
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      :product_id
+    )
   end
 end
