7c0424c2e5d8a545738f0e2713d8ecdc72a6b2be
pnolan89 <petenol@gmail.com>
2019-02-25 20:31:03 +0000

    add error message tests for product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,11 +9,31 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
+      expect(subject.errors.full_messages.length).to eql(0)
     end
 
     it 'is not valid without a name' do
       subject.name = nil
       expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Name can't be blank")
+    end
+
+    it 'is not valid without a price' do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Price cents is not a number")
+    end
+
+    it 'is not valid without a quantity' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Quantity can't be blank")
+    end
+
+    it 'is not valid without a category' do
+      subject.category = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Category can't be blank")
     end
   end
 end
