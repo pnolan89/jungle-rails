3e352c3665157c641836899b9dd5c5e1c111dc70
pnolan89 <petenol@gmail.com>
2019-02-25 22:18:46 +0000

    add more validation tests to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-
-  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates_length_of :password, minimum: 8
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,5 +29,38 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages[0]).to eql("Password confirmation doesn't match Password")
     end
+
+    it 'is not valid without email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Email can't be blank")
+    end
+
+    it 'is not valid without unique email' do
+      User.create(first_name: "John", last_name: "Doe", email: "john@doe",
+                        password: "password", password_confirmation: "password")
+      subject.email = "JOHN@DOE"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Email has already been taken")
+    end
+
+    it 'is not valid with a password less than 8 characters' do
+      subject.password = "pass"
+      subject.password_confirmation = "pass"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Password is too short (minimum is 8 characters)")
+    end
+
+    it 'is not valid without a first_name' do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("First name can't be blank")
+    end
+
+    it 'is not valid without a last_name' do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages[0]).to eql("Last name can't be blank")
+    end
   end
 end
